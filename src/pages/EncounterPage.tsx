100b4a45e4a9658a98012a0e5cf1f3c58093e50e
Darkstrike <sayantandas579pc@gmail.com>
2026-05-19 18:55:31 +0530

    Add files via upload

---
+++ src/pages/EncounterPage.tsx
@@ -0,0 +1,9 @@
+import EncounterGame from '../components/game/EncounterGame'
+
+export default function EncounterPage() {
+  return (
+    <div className="min-h-[calc(100vh-64px)] bg-app-background bg-noise">
+      <EncounterGame />
+    </div>
+  )
+}
